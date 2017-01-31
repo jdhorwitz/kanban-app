de4eec9b43d9c0b4840ac5c772b16fb0e01c0125
Josh Horwitz <joshua.d.horwitz@gmail.com>
2017-01-30 19:07:01 -0800

    Updated

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import uuid from 'uuid';
 import Notes from './Notes';
-import connect from '../../libs/connect';
+import connect from '../libs/connect';
 
 class App extends React.Component {
   constructor(props) {
@@ -12,12 +12,13 @@ class App extends React.Component {
         {
           id: uuid.v4(),
           task: 'Learn React'
-        }, {
+        },
+        {
           id: uuid.v4(),
           task: 'Do laundry'
         }
       ]
-    };
+    }
   }
   render() {
     const {notes} = this.state;
@@ -30,60 +31,58 @@ class App extends React.Component {
           notes={notes}
           onNoteClick={this.activateNoteEdit}
           onEdit={this.editNote}
-          onDelete={this.deleteNote}/>
+          onDelete={this.deleteNote}
+          />
       </div>
     );
   }
   addNote = () => {
+    // It would be possible to write this in an imperative style.
+    // I.e., through `this.state.notes.push` and then
+    // `this.setState({notes: this.state.notes})` to commit.
+    //
+    // I tend to favor functional style whenever that makes sense.
+    // Even though it might take more code sometimes, I feel
+    // the benefits (easy to reason about, no side effects)
+    // more than make up for it.
+    //
+    // Libraries, such as Immutable.js, go a notch further.
     this.setState({
-      notes: this
-        .state
-        .notes
-        .concat([
-          {
-            id: uuid.v4(),
-            task: 'New Task'
-          }
-        ])
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New task'
+      }])
     });
   }
   deleteNote = (id, e) => {
+    // Avoid bubbling to edit
     e.stopPropagation();
 
     this.setState({
-      notes: this
-        .state
-        .notes
-        .filter(note => note.id !== id)
+      notes: this.state.notes.filter(note => note.id !== id)
     });
   }
   activateNoteEdit = (id) => {
     this.setState({
-      notes: this
-        .state
-        .notes
-        .map(note => {
-          if (note.id === id) {
-            note.editing = true;
-          }
+      notes: this.state.notes.map(note => {
+        if(note.id === id) {
+          note.editing = true;
+        }
 
-          return note;
-        })
+        return note;
+      })
     });
   }
   editNote = (id, task) => {
     this.setState({
-      notes: this
-        .state
-        .notes
-        .map(note => {
-          if (note.id === id) {
-            note.editing = false;
-            note.task = task;
-          }
+      notes: this.state.notes.map(note => {
+        if(note.id === id) {
+          note.editing = false;
+          note.task = task;
+        }
 
-          return note;
-        })
+        return note;
+      })
     });
   }
 }
